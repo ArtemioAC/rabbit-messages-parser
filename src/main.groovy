d677b079df4677eb9c73ce686c05ae543483779e
Alvarado <artemio.alvarado@audatex.com>
2021-06-02 13:38:45 -0500

    Simple groovy script to parse log outputs and retrieve RabbitMQ messages

---
+++ src/main.groovy
@@ -0,0 +1,23 @@
+import groovy.io.FileType
+
+def pattern = '{"bucket'
+def dir = new File("../src/resources/")
+
+dir.eachFileRecurse (FileType.FILES) { file ->
+    println "Found file: ${file.name}"
+
+    def outputFile = new File("../src/output/", "Messages_${file.name}")
+    outputFile.write("")
+
+    def messagesNumber = 0
+    file.eachLine {line ->
+        if (line.contains(pattern)){
+            def message = line.split("\\{")[1]
+            println "{${message}"
+            outputFile.append("{${message}\n")
+            messagesNumber++
+        }
+    }
+    outputFile.append("Found ${messagesNumber} messages\n")
+}
+
